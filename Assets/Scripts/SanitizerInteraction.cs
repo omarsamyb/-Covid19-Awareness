13e39286dac681d1dea2d810075c135bbace69a1
dondo98 <47148774+dondo98@users.noreply.github.com>
2020-12-11 23:50:33 +0200

    finalized sanitizing animation

--- Assets/Scripts/SanitizerInteraction.cs
+++ Assets/Scripts/SanitizerInteraction.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 
 public class SanitizerInteraction : Interactable
 {
-
+    private Animator playerAnimator;
     public override void Interact()
     {
         base.Interact();
         GameManager.instance.controlsEnabled = false;
-        // timer 1 second 
+        playerAnimator = player.GetComponent<Animator>();
+       // Vector3 newPosition = new Vector3(playerPositionX, 1.0F, playerPositionZ);
+       // player.transform.position = newPosition;
+       // player.transform.rotation
 
-        GameManager.instance.controlsEnabled = true;
-
-
+        playerAnimator.SetBool("isSanitizing", true);
+        StartCoroutine(Wait(4.0f));
+    }
+    private IEnumerator Wait(float waitTime)
+    {
+       
+            yield return new WaitForSeconds(waitTime);
+            print("WaitAndPrint " + Time.time);
+            playerAnimator.SetBool("isSanitizing", false);
+            GameManager.instance.controlsEnabled = true;
     }
 }
