0d96aa2e4e034d1d8767fc6f5fed215aeacce873
dondo98 <47148774+dondo98@users.noreply.github.com>
2020-12-11 22:29:45 +0200

    add sanitizing

---
+++ Assets/Scripts/SanitizerInteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SanitizerInteraction : Interactable
+{
+
+    public override void Interact()
+    {
+        base.Interact();
+        GameManager.instance.controlsEnabled = false;
+        // timer 1 second 
+
+        GameManager.instance.controlsEnabled = true;
+
+
+    }
+}
