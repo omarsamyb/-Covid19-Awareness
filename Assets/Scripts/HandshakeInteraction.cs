2faf5f464295343b7307fa5a40326587b3601be2
omarsamyb <omarsamyb@gmail.com>
2020-12-13 01:35:54 +0200

    start scene finished

--- Assets/Scripts/HandshakeInteraction.cs
+++ Assets/Scripts/HandshakeInteraction.cs
@@ -22,10 +22,16 @@ public class HandshakeInteraction : Interactable
         {
             yield return null;
         }
-        while (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.ShakingHands") || npcAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.ShakingHands"))
+        while ((playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.ShakingHands") && playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.5f) || (npcAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.ShakingHands") && npcAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.5f))
         {
             yield return null;
         }
+        playerAnimator.SetBool("isTalking", true);
+        npcAnimator.SetBool("isTalking", true);
+        while (AudioManager.instance.isPlaying("GreetingsSFX"))
+            yield return null;
+        playerAnimator.SetBool("isTalking", false);
+        npcAnimator.SetBool("isTalking", false);
         finished = true;
     }
 }
--- Assets/Scripts/HugInteraction.cs
+++ Assets/Scripts/HugInteraction.cs
@@ -36,10 +36,16 @@ public class HugInteraction : Interactable
         {
             yield return null;
         }
-        while (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Hugging") || npcAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Hugging"))
+        while ((playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Hugging") && playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.5f) || (npcAnimator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Hugging") && npcAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.5f))
         {
             yield return null;
         }
+        playerAnimator.SetBool("isTalking", true);
+        npcAnimator.SetBool("isTalking", true);
+        while (AudioManager.instance.isPlaying("GreetingsSFX"))
+            yield return null;
+        playerAnimator.SetBool("isTalking", false);
+        npcAnimator.SetBool("isTalking", false);
         finished = true;
     }
 }
