43ed612b65f15be49b63f83060abcab47d345436
omarsamyb <omarsamyb@gmail.com>
2020-12-13 18:01:04 +0200

    tv interaction + some navmesh modifications

--- Assets/Scripts/TVInteraction.cs
+++ Assets/Scripts/TVInteraction.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class TVInteraction : Interactable
 {
     public Transform directionTransform;
     private Animator playerAnimator;
+    public GameObject playerVC;
+    public GameObject tvVC;
+    public Transform crosshair;
+    public GameObject videoFX;
+    private VideoPlayer vp;
     public override void Interact()
     {
         base.Interact();
         GameManager.instance.controlsEnabled = false;
         playerAnimator = player.GetComponent<Animator>();
-        StartCoroutine(WatchTV());
+        vp = videoFX.GetComponent<VideoPlayer>();
+        StartCoroutine(Rotate());
     }
 
-    IEnumerator WatchTV()
+    IEnumerator Rotate()
     {
         Quaternion initialRotation = player.rotation;
         Vector3 direction = (directionTransform.position - interactionTransform.position).normalized;
@@ -26,10 +32,36 @@ public class TVInteraction : Interactable
         }
         direction = (directionTransform.position - interactionTransform.position).normalized;
         player.rotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
-
         playerAnimator.SetBool("isSitting", true);
-        yield return new WaitForSeconds(3);
+        StartCoroutine(WatchTV());
+    }
+    IEnumerator WatchTV()
+    {
+        yield return new WaitForSeconds(2);
+        tvVC.SetActive(true);
+        playerVC.SetActive(false);
+        crosshair.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1);
+        videoFX.SetActive(true);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(WaitForVideo());
+    }
+    IEnumerator WaitForVideo()
+    {
+        while (vp.isPlaying)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+                break;
+            yield return null;
+        }
+        videoFX.SetActive(false);
+        yield return new WaitForSeconds(1);
         playerAnimator.SetBool("isSitting", false);
-        playerAnimator.SetTrigger("isStanding");
+        GameManager.instance.controlsEnabled = true;
+        playerVC.SetActive(true);
+        tvVC.SetActive(false);
+        crosshair.gameObject.SetActive(true);
+        finished = true;
+        OutcomeManager.instance.Disable_TvInteraction();
     }
 }
